b1ee594dce244cc9cfb179b029102893d38105a4
Debbie-Boat <ddb6@kent.ac.uk>
2026-02-09 19:18:11 +0000

    init_database done

---
+++ fleet_manager.py
@@ -0,0 +1,9 @@
+
+
+def init_database():
+    n = ["Picard", "Riker", "Data", "Worf", "Spock"]
+    r = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
+    d = ["Command", "Command", "Operations", "Security", "Command", "Operations"]
+    i = ["1", "2", "3", "4", "5"]
+
+    return n, r, d, i

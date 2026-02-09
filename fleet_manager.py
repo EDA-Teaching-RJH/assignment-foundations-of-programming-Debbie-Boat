a9969c4f909abb68749951a3b18d3f6afdece060
Debbie-Boat <ddb6@kent.ac.uk>
2026-02-09 19:24:10 +0000

    display_menu done

--- fleet_manager.py
+++ fleet_manager.py
@@ -7,3 +7,19 @@ def init_database():
     i = ["1", "2", "3", "4", "5"]
 
     return n, r, d, i
+
+def display_menu():
+    print("")
+    print("--- MENU ---")
+    print("1) Display Roster")
+    print("2) Add Crew Member")
+    print("3) Remove Crew Member")
+    print("4) Update Crew Rank")
+    print("5) Update Crew Name")
+    print("6) Filter By Division")
+    print("7) Calculate Crew Payroll")
+    print("8) Count Officers")
+
+    option = input("Select Option: ")
+
+    return option
